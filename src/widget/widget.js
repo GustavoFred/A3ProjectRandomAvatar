4f2627d7200712455016e2b5498e4e5627dfd125
Gustavo Angelozi Frederico <gustavofrederico99@gmail.com>
2024-11-08 21:20:24 -0300

    Added filter.

--- src/widget/widget.js
+++ src/widget/widget.js
@@ -1,16 +1,46 @@
 import { getColorIterator } from "./utils/colors/color.js"
 import sample from "./examples/widget00.js"
+import blob from "./utils/blob/blob.js"
+import shape from "./utils/shape/shape.js"
+import rects from "./utils/grids/grid01.js"
 
 function widget(key, draw) {
     
     let nextColor = getColorIterator(key)
 
-    draw.rect().size(500,500).move(250,250).fill(nextColor())
-    draw.circle().size(350).move(400,325).fill(nextColor()).opacity(0.5)
-    draw.circle().size(350).move(250,325).fill(nextColor()).opacity(0.5)
+    const size = 700
+    const cubeSize = 400
+
+    draw.rect().size(1000,1000).fill(nextColor()).opacity(0.2)
+
+    for(let i = 0; i < 4; i++) {
+        let b = blob(key.next256())
+        b.fill(nextColor()).opacity(0.5)
+
+        let height =  (i % 2 == 0)? -100 : 500 
+        let width = (i<2)? -100 : 500 
+
+        b.move(width,height).size(size)
+
+        b.addTo(draw)
+    }
+
+    let s = shape(37)
+    let scolor = nextColor()
+    s.fill(scolor).opacity(0.8)
+    s.move(500-(cubeSize/2), 500-(cubeSize/2)).size(cubeSize)
+    s.addTo(draw)        
     
-    // Descomente linha abaixo para ver exemplo 0
-    //sample(key,draw)
+    let s2 = shape(key.next256())
+    let s2color = nextColor()
+
+    while (scolor === s2color)
+        s2color = nextColor()
+
+    s2.fill(s2color).opacity(1)
+    s2.move(520-(cubeSize/2), 520-(cubeSize/2)).size(cubeSize-40, cubeSize-40)
+    s2.addTo(draw) 
+    
 }
 
 export default widget
